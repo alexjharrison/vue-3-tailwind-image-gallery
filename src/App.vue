62db9db73a2b9a446a3715453647de7f743e2b0c
Alex Harrison <alexjharrison@gmail.com>
2020-07-25 16:27:52 -0400

    initial config done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="text-yellow-400 bg-blue-600 h-screen">
+    {{ thing }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld
+  setup() {
+    const thing = process.env.VUE_APP_TEST_THING;
+    return {
+      thing
+    };
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
